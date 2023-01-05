0a26960f5792421146033bf2ddbf77d6efe2e40e
Vladislav <75188379+Unexperiance1student@users.noreply.github.com>
2023-01-06 01:23:10 +0300

    Update World
    add import (иду Адыхать)

--- sections/World.jsx
+++ sections/World.jsx
@@ -1,9 +1,11 @@
 'use client';
 
-const World = () => (
-  <section>
-    World section
-  </section>
-);
+import { motion } from 'framer-motion';
+import { NewFeatures, TitleText, TypingText } from '../components';
+import styles from '../styles';
+import { staggerContainer, fadeIn, planetVariants } from '../utils/motion';
+import { newFeatures } from '../constants';
+
+const World = () => <section>World section</section>;
 
 export default World;
